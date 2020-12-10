2988232ff07a6b5940500b032286b18047c9fb78
Billy Vong <billy@sentry.io>
2020-12-10 15:09:42 -0800

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,121 +1,75 @@
-import * as path from 'path'
+import fs from 'fs';
 
-import * as core from '@actions/core'
-import {exec} from '@actions/exec'
-import * as github from '@actions/github'
-
-import {getChangedFiles} from './getChangedFiles'
+import * as core from '@actions/core';
+import {exec} from '@actions/exec';
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-    const {owner, repo} = github.context.repo
-    const dryRun = core.getInput('dry')
-    const token = core.getInput('GITHUB_TOKEN')
+    const {owner, repo} = github.context.repo;
+    const token = core.getInput('github-token');
 
     if (!token) {
-      core.debug(`NO GITHUB_TOKEN`)
+      core.debug(`Invalid GitHub token`);
     }
 
-    const octokit = new github.GitHub(token)
+    const octokit = github.getOctokit(token);
 
-    const changedFiles = await getChangedFiles(octokit)
-    core.debug(changedFiles.join(', '))
+    let gitOutput = '';
+    let gitError = '';
 
-    if (!changedFiles.length) {
-      return
-    }
-
-    let results: any = []
-
-    let eslintOutput = ''
-    let eslintError = ''
     try {
-      await exec(
-        'node',
-        [
-          path.join(process.cwd(), 'node_modules/eslint/bin/eslint'),
-          `--fix-dry-run`,
-          '--format',
-          'json',
-          ...changedFiles
-        ],
-        {
-          silent: true,
-          listeners: {
-            stdout: (data: Buffer) => {
-              eslintOutput += data.toString()
-            },
-            stderr: (data: Buffer) => {
-              eslintError += data.toString()
-            }
-          }
-        }
-      )
+      await exec('git', ['ls-files', '-m'], {
+        silent: true,
+        listeners: {
+          stdout: (data: Buffer) => {
+            gitOutput += data.toString();
+          },
+          stderr: (data: Buffer) => {
+            gitError += data.toString();
+          },
+        },
+      });
     } catch {}
 
-    core.debug(`error running eslint?: ${eslintError}`)
+    core.debug(`error?: ${gitError}`);
 
-    try {
-      results = JSON.parse(eslintOutput.replace(/\\"/g, '\\"'))
-      const stylish = require('eslint/lib/formatters/stylish')
+    const files = gitOutput.split('\n');
 
-      // log to console so github action problem matchers can work on output
-      console.log(stylish(results)) // eslint-disable-line no-console
-
-      if (results.find(({errorCount}: any) => errorCount > 0)) {
-        core.setFailed('eslint completed with errors')
-      }
-    } catch (err) {
-      core.debug(eslintOutput)
-      core.setFailed(err.message)
-    }
-
-    if (dryRun) {
-      return
-    }
-
-    for (const result of results) {
-      const filePath = result.filePath.replace(
-        `${process.env.GITHUB_WORKSPACE}/`,
-        ''
-      )
-
-      let file
-
-      if (result.output) {
-        try {
-          core.debug(`getContents: ${filePath}`)
-          const {data} = await octokit.repos.getContents({
-            owner,
-            repo,
-            path: filePath,
-            ref: process.env.GITHUB_HEAD_REF
-          })
-          file = data
-        } catch (err) {
-          core.debug(err.message)
-        }
-
-        if (!file || Array.isArray(file)) {
-          return
-        }
-
-        // Commit eslint fixes
-        octokit.repos.createOrUpdateFile({
+    for (const path of files) {
+      try {
+        const {data: ghFileContent} = await octokit.repos.getContent({
           owner,
           repo,
-          path: filePath,
-          sha: file.sha,
+          path,
+          ref: process.env.GITHUB_HEAD_REF,
+        });
+
+        if (!ghFileContent || Array.isArray(ghFileContent)) {
+          return;
+        }
+
+        const fileContent = fs.readFileSync(path);
+
+        // Commit eslint fixes
+        octokit.repos.createOrUpdateFileContents({
+          owner,
+          repo,
+          path,
           message: 'style(): Auto eslint fix',
-          content: Buffer.from(result.output).toString('base64'),
-          branch: process.env.GITHUB_HEAD_REF
-        })
+          sha: ghFileContent.sha,
+          content: Buffer.from(fileContent).toString('base64'),
+          branch: process.env.GITHUB_HEAD_REF,
+        });
+      } catch (error) {
+        core.error(error);
+        core.setFailed(error);
       }
     }
   } catch (error) {
-    core.debug(error.stack)
-    core.setFailed(error.message)
+    core.debug(error.stack);
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
